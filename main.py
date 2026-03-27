5efff818a6879de79b9da601f07e48e71d18a753
Ashrith Akhandam <ashrithakhandam@Ashriths-MacBook-Air.local>
2026-03-27 18:34:24 +0530

    Stored subject data in list

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-
 print("Welcome to Smart Study Planner")
+
+subjects = []
 
 n = int(input("Enter number of subjects: "))
 
 for i in range(n):
     name = input(f"Enter subject {i+1} name: ")
- 
+    difficulty = int(input("Enter difficulty (1-5): "))
+    days_left = int(input("Days left for exam: "))
+
+    subject = {
+        "name": name,
+        "difficulty": difficulty,
+        "days_left": days_left
+    }
+
+    subjects.append(subject)
+
+print(subjects)
 
 
 
